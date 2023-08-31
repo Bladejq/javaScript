ba5585e6a25fff85d4b80d394b0b603883faaeec
Bladejq <121391744+Bladejq@users.noreply.github.com>
2023-09-01 00:34:43 +0600

    Commit 2

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log("Hello world");
+// 1 тапсырма
+
+let clothes = [];
+
+clothes.push('Шапка');
+clothes.push('Джинсы');
+clothes.push('Рубашка');
+clothes.push('Шорты');
+clothes.push('Шалстук');
+
+clothes.pop();
+
+console.log(clothes);
+
+// 2 тапсырма
+
+LetterFinder = () => {
+    console.log("Word");
+    console.log("Match");
+}
+LetterFinder()
+
+// 3 тапсырма
+let word = "Nurnaz";
+
+for (let i = 0; i < word.length; i++) {
+    console.log(word[i]);
+}
+
+// 4 тапсырма
+num = [5, 6, 4, 5, 8, 10];
+
+let index_sum=() => {
+    let s = 0;
+    for(let i =0; i<num.length;i++) {
+        if(i%2 == 0) {
+            s+=num[i];
+        }
+    }
+    console.log(s);
+}
+index_sum();
+
+// 5 тапсырма
+const arr = [1,2,3,4,5];
+console.log(arr.reverse());
