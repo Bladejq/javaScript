3f32fad374cb3707bcf106959435e473d3484881
Bladejq <nurnazsherkhan06@gmail.com>
2023-09-12 08:23:20 +0600

    Commit 2

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ c = [33,44,55,0,11,7,8,9];
 
 let qos = 0;
 let kob = 1;
-let a;
+let k;
 for (var i = 0; i < c.length; i++) {
-    a = c[i];
-    if (a === 0) {break;}
-    qos+=a;}
+    k = c[i];
+    if (k === 0) {break;}
+    qos+=k;}
 for(var j = i+1;j<c.length;j++){
-    a = c[j];
-    kob*=a;}
+    k = c[j];
+    kob*=k;}
 console.log(qos);
 console.log(kob);
 
@@ -72,10 +72,39 @@ for(i in shop){
 
 // 4 Тапсырма
 
-let massive = ["Tom", "Number", "Car", "Lambargini"]
-console.log(massive.pop("CS:GO"));
+let a = [
+    {id:1,name:"TEST"}
+];
+function addData(id,name){
+    var user1 = {id:2,name:"QWERTY"};
+    var user2 = {id:3,name:"DOG"};
+    a.push(user1);
+    a.push(user2);
+}
+addData(4,"ASDFGHJ");
 
-let cityInput = prompt("Қала атауын жазыңыз");
+function updData(id,newName){
+    var upd = a.find(user =>user.id === id);
+    if(upd){
+        upd.name = newName;
+    }
+}
+
+updData(2,"NewName");
+
+function deleteData(id){
+    a = a.filter(user => user.id !==id);
+}
+
+deleteData(1);
+
+for(let i =0;i<a.length;i++){
+    console.log(a[i]);
+}
+
+// 5 тапсырма
+
+
 
 let city = {
     "city1":"Almaty",
@@ -95,14 +124,95 @@ let regionAqtau = {
     "region1":"Tasilinskii"
 }
 
-for(let i in regionAlmaty) {
-if (cityInput === city.city1) {
-        alert(regionAlmaty[i]);
-    } else if (cityInput === city.city2){
-        alert(regionAstana[i]);
-    } else if (cityInput === city.city3) {
-        alert(regionAqtau[i]);
+let vscode = () => {
+    let cityInput = prompt("Қала атауын жазыңыз");
+
+    if (cityInput == city.city1) {
+        for (let i in regionAlmaty) {
+            alert(i + " " + regionAlmaty[i])
+        }
+    } else if (cityInput == city2.city2) {
+        for (let j in regionAstana) {
+            alert(j + " " + regionAstana[j])
+        }
+    } else if (cityInput == city.city3) {
+        for (let s in regionAqtau) {
+            alert(s + " " + regionAqtau)
+        }
+    }
+} 
+
+vscode() 
+
+for (let i = 0; i < 100; i++) {
+    console.log("Hello, world!");
+}
+
+
+// Тест
+// TEST
+// 1 esep
+
+sum = (c2, c1) => {
+    return c2 + c1;
+  }
+  
+console.log(sum(70, 80)); 
+
+
+//  2 есеп
+
+bir = ['kitap'];
+eki = ['khana'];
+
+ 
+let qosu = () => {
+    console.log(bir + eki);
+}
+qosu();
+
+// 3 esep 
+
+ush_massive = [1, 5, 7, 9, 6, 8]
+console.log(ush_massive.indexOf(5));
+
+//  4 esep 
+let massiv4ik = [
+    {id:1,name:"TEST"},
+];
+
+
+addData = (id, name) => {
+    let user1 = {id:2,name:"TEST2"};
+    let user2 = {id:3,name:"TEST3"}
+    massiv4ik.push(user1);
+    massiv4ik.push(user2);
+}
+addData(3, "TEST3")
+
+function deleteData(id) {
+    massiv4ik = massiv4ik.filter(user => user.id !==id);
+}
+deleteData(2);
+
+for(let i =0;i<massiv4ik.length;i++){
+    console.log(massiv4ik[i]);
+}
+
+
+// 5 esep 
+
+let san1 = 5;
+let san2 = 9;
+
+if (san1 > san2){
+    console.log(san1);
 } else {
-    alert('Қате');
+    console.log(san2);
 }
+
+// 6 esep
+for(let i = 1; i <= 10; i++){
+console.log(i);
 }
+
